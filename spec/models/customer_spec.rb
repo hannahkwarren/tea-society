20719877a1b35d4c02158d2222208f3f3ea8b1f8
Hannah Warren <hannahkwarren@gmail.com>
2022-04-11 20:52:44 -0400

    Fix formatting, thanks Rubocop

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe Customer, type: :model do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
     it { should validate_presence_of(:email) }
-    it { should allow_value("email@address.foo").for(:email) }
-    it { should_not allow_value("foo").for(:email) }
+    it { should allow_value('email@address.foo').for(:email) }
+    it { should_not allow_value('foo').for(:email) }
     it { should validate_presence_of(:address) }
     it { should validate_presence_of(:city) }
     it { should validate_presence_of(:state) }
