0b5a60678fd8fac1045d2ad3241eefa456c5e836
Hannah Warren <hannahkwarren@gmail.com>
2022-04-14 10:55:10 -0400

    Adds sad path error handling for customer's subscriptions when customer does not exist

--- app/controllers/api/v1/customer_subscriptions_controller.rb
+++ app/controllers/api/v1/customer_subscriptions_controller.rb
@@ -1,5 +1,7 @@
 # app/controllers/api/v1/customer_subscriptions_controller.rb
 class Api::V1::CustomerSubscriptionsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_error
+
   def show
     customer = Customer.find(params[:id])
     subscriptions = customer.subscriptions
@@ -9,5 +11,11 @@ class Api::V1::CustomerSubscriptionsController < ApplicationController
     else
       render json: subscriptions.errors.details, status: 400
     end
+  end
+
+  private
+
+  def handle_error(error)
+    render json: { error: error.to_s }, status: :bad_request
   end
 end
--- spec/requests/customer_subscriptions_spec.rb
+++ spec/requests/customer_subscriptions_spec.rb
@@ -2,8 +2,8 @@
 require 'rails_helper'
 
 RSpec.describe "Customer's subscriptions", type: :request do
-  it "provides a list of a customer's subscriptions" do
-    cust1 = Customer.create(first_name: 'Hannah',
+  before(:each) do
+    @cust1 = Customer.create(first_name: 'Hannah',
                             last_name: 'Warner',
                             email: 'hannah@mail.com',
                             address: '123 Main St',
@@ -11,12 +11,12 @@ RSpec.describe "Customer's subscriptions", type: :request do
                             state: 'NY',
                             zip: '11206')
 
-    tea1 = Tea.create(  name: 'Sun Moon Lake Hong Cha',
+    @tea1 = Tea.create(  name: 'Sun Moon Lake Hong Cha',
                         description: 'Light, naturally sweet and floral oolong tea',
                         temperature_fahrenheit: 190,
                         brew_time_mins: 4)
 
-    tea2 = Tea.create(  name: 'Antioxidant Green',
+    @tea2 = Tea.create(  name: 'Antioxidant Green',
                         description: 'Grassy and spinach green',
                         temperature_fahrenheit: 180,
                         brew_time_mins: 3)
@@ -24,16 +24,18 @@ RSpec.describe "Customer's subscriptions", type: :request do
     Subscription.create(title: 'Monthly Fix',
                         price: 25.99,
                         frequency: 'monthly',
-                        customer_id: cust1.id,
-                        tea_id: tea1.id)
+                        customer_id: @cust1.id,
+                        tea_id: @tea1.id)
 
     Subscription.create(title: 'Occasional Mellow',
                         price: 29.99,
                         frequency: 'every_three_months',
-                        customer_id: cust1.id,
-                        tea_id: tea2.id)
+                        customer_id: @cust1.id,
+                        tea_id: @tea2.id)
+  end
 
-    get "/api/v1/customer/#{cust1.id}/subscriptions"
+  it "happy path: provides a list of a customer's subscriptions" do
+    get "/api/v1/customer/#{@cust1.id}/subscriptions"
 
     expect(response).to be_successful
     parsed = JSON.parse(response.body, symbolize_names: true)
@@ -45,5 +47,13 @@ RSpec.describe "Customer's subscriptions", type: :request do
     expect(parsed[:data][0][:attributes]).to have_key(:price)
     expect(parsed[:data][0][:attributes]).to have_key(:customer_id)
     expect(parsed[:data][0][:attributes]).to have_key(:tea_id)
+  end
+
+  it 'sad path: returns error when customer not found' do
+    get "/api/v1/customer/#{1002}/subscriptions"
+
+    expect(response).to have_http_status(400)
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    expect(parsed[:error]).to eq("Couldn't find Customer with 'id'=1002")
   end
 end
