4fec0a89c1f44a5c65c320449bfa0a3022275713
Hannah Warren <hannahkwarren@gmail.com>
2022-04-14 10:54:42 -0400

    Adds a couple more teas to db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-cust1 = Customer.create(first_name: 'Hannah',
-  last_name: 'Warner',
-  email: 'hannah@mail.com',
-  address: '123 Main St',
-  city: 'New York',
-  state: 'NY',
-  zip: '11206')
+Customer.create(first_name: 'Hannah',
+                last_name: 'Warner',
+                email: 'hannah@mail.com',
+                address: '123 Main St',
+                city: 'New York',
+                state: 'NY',
+                zip: '11106')
 
-tea1 = Tea.create( name: 'Sun Moon Lake Hong Cha',
-description: 'Light, naturally sweet and floral oolong tea',
-temperature_fahrenheit: 190,
-brew_time_mins: 4
-    )
+Tea.create( name: 'Sun Moon Lake Hong Cha',
+            description: 'Light, naturally sweet and floral oolong tea',
+            temperature_fahrenheit: 190,
+            brew_time_mins: 4
+          )
+
+Tea.create( name: 'Green Monster Matcha',
+            description: 'Vegetal and grass-like',
+            temperature_fahrenheit: 180,
+            brew_time_mins: 3
+          )
+
+Tea.create( name: 'Pai Mu Tan White Tea',
+            description: 'Light, clean and mildly sweet on the finish',
+            temperature_fahrenheit: 190,
+            brew_time_mins: 4)
