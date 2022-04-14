01fca07fadd80ce9a1497d5d2ea30f08b4c3d650
Hannah Warren <hannahkwarren@gmail.com>
2022-04-14 11:48:38 -0400

    Add better error handling and edge case test for creating subscriptions

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,13 +1,20 @@
 # app/controllers/api/v1/subscriptions_controller.rb
 class Api::V1::SubscriptionsController < ApplicationController
-  def create
-    subscription = subscription_params
-    new_subscription = Subscription.new(subscription)
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_error
 
-    if new_subscription.save
-      render json: SubscriptionSerializer.new(new_subscription)
+  def create
+    customer = Customer.find(subscription_params[:customer_id])
+    tea = Tea.find(subscription_params[:tea_id])
+    subscription = subscription_params
+    if customer.present? && tea.present?
+      new_subscription = Subscription.new(subscription)
+      if new_subscription.save
+        render json: SubscriptionSerializer.new(new_subscription)
+      else
+        render json: new_subscription.errors.details, status: 400
+      end
     else
-      render json: new_subscription.errors.details, status: 400
+      render json: { error: 'Invalid request.' }
     end
   end
 
@@ -21,6 +28,8 @@ class Api::V1::SubscriptionsController < ApplicationController
       else
         render json: subscription.errors.details, status: 400
       end
+    else
+      render json: customer.errors.details, status: 400
     end
   end
 
@@ -28,5 +37,9 @@ class Api::V1::SubscriptionsController < ApplicationController
 
   def subscription_params
     params.require(:subscription).permit(:id, :title, :price, :status, :frequency, :customer_id, :tea_id)
+  end
+
+  def handle_error(error)
+    render json: { error: error.to_s }, status: :bad_request
   end
 end
--- spec/requests/create_subscription_spec.rb
+++ spec/requests/create_subscription_spec.rb
@@ -40,8 +40,35 @@ RSpec.describe 'Create Subscription', type: :request do
     expect(parsed[:data][:attributes]).to have_key(:tea_id)
   end
 
-  it 'edge case: missing data' do
+  it 'edge case: missing data should result in error' do
+    Customer.create(first_name: 'Hannah',
+                            last_name: 'Warner',
+                            email: 'hannah@mail.com',
+                            address: '123 Main St',
+                            city: 'New York',
+                            state: 'NY',
+                            zip: '11206')
 
+    tea1 = Tea.create(  name: 'Sun Moon Lake Hong Cha',
+                        description: 'Light, naturally sweet and floral oolong tea',
+                        temperature_fahrenheit: 190,
+                        brew_time_mins: 4
+                      )
+
+    json_payload = { subscription: {
+                      title: 'Monthly Fix',
+                      price: 25.99,
+                      frequency: 'monthly',
+                      tea_id: tea1.id
+                      # missing customer id
+                      }
+                    }
+
+    post '/api/v1/subscriptions', params: json_payload
+
+    expect(response).to have_http_status(400)
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed[:error]).to eq("Couldn't find Customer without an ID")
   end
-
 end
